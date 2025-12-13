cf36ec59b8abc802c11142a70d35771dd9ef74c9
Marc <johnfelixmarc@gmail.com>
2025-12-13 18:51:31 +0800

    feat: profile page

---
+++ src/app/dashboard/landlord/profile/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { UserProfile } from "@/components/user-profile";
+
+export default function ProfilePage() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <UserProfile />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
